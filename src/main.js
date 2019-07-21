6afa7bd5c1345aef29d910916ffb17da178aceb1
Daynizyvc <zinyad_leay@hotmail.com>
2019-07-21 13:26:04 -0500

    Se termino de hacer el maquetado para mobile y desktop

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ let toggleMenu = (event) =>{
 }
 menu.addEventListener('click', toggleMenu, false);
 
-google.load('maps','2',{callback:simple});var map;
-  function simple(){
-     var map = new GMap2(document.getElementById("show-map"));
-     map.setCenter(new GLatLng(19.421523, -99.163199),4);
-};
+  google.load('maps','2',{callback:simple});var map;
+    function simple(){
+       var map = new GMap2(document.getElementById("show-map"));
+       map.setCenter(new GLatLng(19.421523, -99.163199),4);
+ };
