2c33025ee2553e7bd3a6453ee4ac4454c1844864
Daynizyvc <zinyad_leay@hotmail.com>
2019-07-20 23:41:49 -0500

    Se esta trabajando a la par el mobile y el desktop

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
+let menu= document.querySelector('.main-hamburger');
+
+let toggleMenu = (event) =>{
+    menu.classList.toggle('is-active');
+    document.querySelector(".menu-app").classList.toggle("is_active");
+    event.preventDefault();
+}
+menu.addEventListener('click', toggleMenu, false);
+
 google.load('maps','2',{callback:simple});var map;
- function simple(){
-    var map = new GMap2(document.getElementById("show-map"));
-    map.setCenter(new GLatLng(19.421523, -99.163199),4);
+  function simple(){
+     var map = new GMap2(document.getElementById("show-map"));
+     map.setCenter(new GLatLng(19.421523, -99.163199),4);
 };
