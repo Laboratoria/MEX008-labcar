865265f574d2525a3593ac0f081c8e3cc6dd9d58
Daynizyvc <zinyad_leay@hotmail.com>
2019-07-22 14:34:45 -0500

    se agrega el primer modal

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
+const sesion=document.getElementById("inicia-sesion");
+const modalSesion=document.getElementById("modal-session");
+const close=document.getElementById("logging");
+
+
+const show = () =>{
+    modalSesion.style.display="flex";
+    modalSesion.style.visibility="visible";
+}
+
+const closeWindow = () =>{
+    modalSesion.style.display = "none";
+    modalSesion.style.visibility="collapse";
+}
+
+sesion.addEventListener('click', show);
+close.addEventListener('click', closeWindow);
+
 let menu= document.querySelector('.main-hamburger');
 
 let toggleMenu = (event) =>{
